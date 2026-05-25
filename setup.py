d641de22aaa9e5f48545ac2f5f5a1c6e9f782e06
artshuu <78686705+artshuu@users.noreply.github.com>
2026-05-25 16:34:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="timesheet-tech-dept",
     version="1.0.0",
-    packages=find_packages(),
+    # Явно указываем, что пакеты находятся внутри папки src
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
